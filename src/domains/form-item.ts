1096e0ae54b6457de3676a94eb1c09c38e23681d
Arathi <ssdfa7@gmail.com>
2024-12-05 11:32:47 +0800

    0.24.1202-alpha+4
    1. 表单项数据定义移到form-item.ts
    2. 调整editor中header和content的大小与边距
    
    Signed-off-by: Arathi <ssdfa7@gmail.com>

--- src/domains/form-item.ts
+++ src/domains/form-item.ts
@@ -2,6 +2,7 @@ import type { SegmentedOptions } from '@/components/Segmented';
 import type { SliderOptions } from '@/components/Slider';
 import type { SwitchOptions } from '@/components/Switch';
 import type OptionSettings from './option-settings';
+import { DeathPenalty } from './option-settings';
 
 type FormItemProps<T = OptionSettings> = {
   name: keyof T;
@@ -24,3 +25,321 @@ interface SwitchItemProps {
 }
 
 export default FormItemProps;
+
+// #region 表单项
+export const DifficutyFormItems: FormItemProps[] = [
+  {
+    name: 'DayTimeSpeedRate',
+    label: '白天流逝速度',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'NightTimeSpeedRate',
+    label: '夜晚流逝速度',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  // TODO 自动保存间隔
+  {
+    name: 'ExpRate',
+    label: '经验值倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 20,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalCaptureRate',
+    label: '捕获概率倍率',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 2,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalSpawnNumRate',
+    label: '帕鲁刷新数量倍率',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 3,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalDamageRateAttack',
+    label: '帕鲁攻击伤害倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalDamageRateDefense',
+    label: '帕鲁承受伤害倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalStomachDecreaceRate',
+    label: '帕鲁饱食度降低倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalStaminaDecreaceRate',
+    label: '帕鲁耐力降低倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalAutoHPRegeneRate',
+    label: '帕鲁生命值自然回复倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalAutoHpRegeneRateInSleep',
+    label: '帕鲁睡眠时生命值回复倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PlayerDamageRateAttack',
+    label: '帕鲁攻击伤害倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PlayerDamageRateDefense',
+    label: '帕鲁承受伤害倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PlayerStomachDecreaceRate',
+    label: '帕鲁饱食度降低倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PlayerStaminaDecreaceRate',
+    label: '帕鲁耐力降低倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PlayerAutoHPRegeneRate',
+    label: '帕鲁生命值自然回复倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PlayerAutoHpRegeneRateInSleep',
+    label: '帕鲁睡眠时生命值回复倍率',
+    type: 'slider',
+    options: {
+      min: 0.1,
+      max: 5,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'BuildObjectDamageRate',
+    label: '对建筑伤害倍率',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 3,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'BuildObjectDeteriorationDamageRate',
+    label: '建筑物的劣化速度倍率',
+    type: 'slider',
+    options: {
+      min: 0,
+      max: 10,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'DropItemMaxNum',
+    label: '世界内的掉落物上限',
+    type: 'slider',
+    options: {
+      min: 0,
+      max: 5000,
+      step: 10,
+    },
+  },
+  {
+    name: 'CollectionDropRate',
+    label: '道具采集量倍率',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 3,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'CollectionObjectHpRate',
+    label: '可采集物品生命值倍率',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 3,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'CollectionObjectRespawnSpeedRate',
+    label: '可采集物品刷新间隔',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 3,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'EnemyDropItemRate',
+    label: '道具掉落量倍率',
+    type: 'slider',
+    options: {
+      min: 0.5,
+      max: 3,
+      step: 0.1,
+    },
+  },
+  {
+    name: 'PalEggDefaultHatchingTime',
+    label: '巨大蛋孵化所需时间（小时）',
+    type: 'slider',
+    options: {
+      min: 0,
+      max: 240,
+      step: 1,
+    },
+  },
+  {
+    name: 'bEnableInvaderEnemy',
+    label: '是否会发生袭击事件？',
+    type: 'switch',
+    options: {
+      off: '否',
+      on: '是',
+    },
+  },
+  {
+    name: 'DeathPenalty',
+    label: '死亡惩罚',
+    type: 'segmented',
+    options: [
+      {
+        value: DeathPenalty.None,
+        label: '不掉落任何东西',
+      },
+      {
+        value: DeathPenalty.Item,
+        label: '掉落装备以外的道具',
+      },
+      {
+        value: DeathPenalty.ItemAndEquipment,
+        label: '掉落所有道具',
+      },
+      {
+        value: DeathPenalty.All,
+        label: '掉落所有道具及队伍内帕鲁',
+      },
+    ],
+  },
+  {
+    name: 'GuildPlayerMaxNum',
+    label: '公会人数上限',
+    type: 'slider',
+    options: {
+      min: 1,
+      max: 100,
+      step: 1,
+    },
+  },
+  {
+    name: 'BaseCampMaxNumInGuild',
+    label: '各公会的最大据点数',
+    type: 'slider',
+    options: {
+      min: 3,
+      max: 10,
+      step: 1,
+    },
+  },
+  {
+    name: 'BaseCampWorkerMaxNum',
+    label: '据点工作帕鲁最大数',
+    type: 'slider',
+    options: {
+      min: 1,
+      max: 50,
+      step: 1,
+    },
+  },
+];
+// #endregion
--- src/routes/editor/page.tsx
+++ src/routes/editor/page.tsx
@@ -2,27 +2,24 @@ import Button from '@/components/Button';
 import Segmented from '@/components/Segmented';
 import Slider from '@/components/Slider';
 import Switch from '@/components/Switch';
-import type FormItemProps from '@/domains/form-item';
-import {
-  DeathPenalty,
-  DefaultOptionSettings,
-  Difficulty,
-} from '@/domains/option-settings';
+import { DifficutyFormItems } from '@/domains/form-item';
+import { DefaultOptionSettings, Difficulty } from '@/domains/option-settings';
 import { useWindowSize } from '@/hooks/window-size';
 import { Helmet } from '@modern-js/runtime/head';
-import { Flex, Form } from 'antd';
+import { Form } from 'antd';
 import { useState } from 'react';
 
 import './index.less';
 
 const Page = () => {
   const windowSize = useWindowSize();
+  const [items] = useState(DifficutyFormItems);
   const [optionSettings, setOptionSettings] = useState(DefaultOptionSettings);
 
   const backgroundSrc =
     'https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1623730/library_hero.jpg';
 
-  const headerHeight = 64;
+  const headerHeight = 48;
   const footerHeight = 64;
 
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -32,325 +29,6 @@ const Page = () => {
       ...changedValues,
     });
   }
-
-  // #region 表单项
-  const items: FormItemProps[] = [
-    {
-      name: 'DayTimeSpeedRate',
-      label: '白天流逝速度',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'NightTimeSpeedRate',
-      label: '夜晚流逝速度',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    // TODO 自动保存间隔
-    {
-      name: 'ExpRate',
-      label: '经验值倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 20,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalCaptureRate',
-      label: '捕获概率倍率',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 2,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalSpawnNumRate',
-      label: '帕鲁刷新数量倍率',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 3,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalDamageRateAttack',
-      label: '帕鲁攻击伤害倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalDamageRateDefense',
-      label: '帕鲁承受伤害倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalStomachDecreaceRate',
-      label: '帕鲁饱食度降低倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalStaminaDecreaceRate',
-      label: '帕鲁耐力降低倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalAutoHPRegeneRate',
-      label: '帕鲁生命值自然回复倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalAutoHpRegeneRateInSleep',
-      label: '帕鲁睡眠时生命值回复倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PlayerDamageRateAttack',
-      label: '帕鲁攻击伤害倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PlayerDamageRateDefense',
-      label: '帕鲁承受伤害倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PlayerStomachDecreaceRate',
-      label: '帕鲁饱食度降低倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PlayerStaminaDecreaceRate',
-      label: '帕鲁耐力降低倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PlayerAutoHPRegeneRate',
-      label: '帕鲁生命值自然回复倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PlayerAutoHpRegeneRateInSleep',
-      label: '帕鲁睡眠时生命值回复倍率',
-      type: 'slider',
-      options: {
-        min: 0.1,
-        max: 5,
-        step: 0.1,
-      },
-    },
-    // ----
-    {
-      name: 'BuildObjectDamageRate',
-      label: '对建筑伤害倍率',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 3,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'BuildObjectDeteriorationDamageRate',
-      label: '建筑物的劣化速度倍率',
-      type: 'slider',
-      options: {
-        min: 0,
-        max: 10,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'DropItemMaxNum',
-      label: '世界内的掉落物上限',
-      type: 'slider',
-      options: {
-        min: 0,
-        max: 5000,
-        step: 10,
-      },
-    },
-    {
-      name: 'CollectionDropRate',
-      label: '道具采集量倍率',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 3,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'CollectionObjectHpRate',
-      label: '可采集物品生命值倍率',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 3,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'CollectionObjectRespawnSpeedRate',
-      label: '可采集物品刷新间隔',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 3,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'EnemyDropItemRate',
-      label: '道具掉落量倍率',
-      type: 'slider',
-      options: {
-        min: 0.5,
-        max: 3,
-        step: 0.1,
-      },
-    },
-    {
-      name: 'PalEggDefaultHatchingTime',
-      label: '巨大蛋孵化所需时间（小时）',
-      type: 'slider',
-      options: {
-        min: 0,
-        max: 240,
-        step: 1,
-      },
-    },
-    {
-      name: 'bEnableInvaderEnemy',
-      label: '是否会发生袭击事件？',
-      type: 'switch',
-      options: {
-        off: '否',
-        on: '是',
-      },
-    },
-    {
-      name: 'DeathPenalty',
-      label: '死亡惩罚',
-      type: 'segmented',
-      options: [
-        {
-          value: DeathPenalty.None,
-          label: '不掉落任何东西',
-        },
-        {
-          value: DeathPenalty.Item,
-          label: '掉落装备以外的道具',
-        },
-        {
-          value: DeathPenalty.ItemAndEquipment,
-          label: '掉落所有道具',
-        },
-        {
-          value: DeathPenalty.All,
-          label: '掉落所有道具及队伍内帕鲁',
-        },
-      ],
-    },
-    {
-      name: 'GuildPlayerMaxNum',
-      label: '公会人数上限',
-      type: 'slider',
-      options: {
-        min: 1,
-        max: 100,
-        step: 1,
-      },
-    },
-    {
-      name: 'BaseCampMaxNumInGuild',
-      label: '各公会的最大据点数',
-      type: 'slider',
-      options: {
-        min: 3,
-        max: 10,
-        step: 1,
-      },
-    },
-    {
-      name: 'BaseCampWorkerMaxNum',
-      label: '据点工作帕鲁最大数',
-      type: 'slider',
-      options: {
-        min: 1,
-        max: 50,
-        step: 1,
-      },
-    },
-  ];
-  // #endregion
 
   const difficultyItems: React.ReactNode[] = items.map(item => {
     let control: React.ReactNode = null;
@@ -404,7 +82,7 @@ const Page = () => {
             }}
             style={{ width: 1280, height: windowSize.height }}
           >
-            <div className="header">
+            <div className="header" style={{ height: headerHeight }}>
               <Form.Item
                 name="Difficulty"
                 label="难度"
@@ -421,9 +99,8 @@ const Page = () => {
               </Form.Item>
             </div>
             <div
-              className="difficulty-settings"
+              className="content"
               style={{
-                // width: 1200,
                 overflowX: 'hidden',
                 overflowY: 'auto',
                 height: windowSize.height - headerHeight - footerHeight,
@@ -431,10 +108,8 @@ const Page = () => {
             >
               {difficultyItems}
             </div>
-            <div className="footer">
-              <Flex>
-                <Button>预览</Button>
-              </Flex>
+            <div className="footer" style={{ height: footerHeight }}>
+              <Button>预览</Button>
             </div>
           </Form>
         </div>
